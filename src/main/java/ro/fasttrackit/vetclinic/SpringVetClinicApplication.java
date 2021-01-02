a09eef58ca26af755026b7033378fb72ffdba532
Chiorean Mihai <mihai.chiorean@iwelcome.com>
2021-01-02 18:19:21 +0200

    spring vet first commit

---
+++ src/main/java/ro/fasttrackit/vetclinic/SpringVetClinicApplication.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.vetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVetClinicApplication.class, args);
+	}
+
+}
