c677bd141fdcbaf2322f9fbce3fbe78d148ad848
Chiorean Mihai <mihai.chiorean@iwelcome.com>
2021-02-12 19:56:54 +0200

    added RabbitMQ

--- src/main/java/ro/fasttrackit/vetclinic/SpringVetClinicApplication.java
+++ src/main/java/ro/fasttrackit/vetclinic/SpringVetClinicApplication.java
@@ -2,8 +2,10 @@ package ro.fasttrackit.vetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringVetClinicApplication {
 
 	public static void main(String[] args) {
